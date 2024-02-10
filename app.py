b07808eecc97b9314d977aff58c08d1a6068c603
Hardik Parmar <121241956+hardikjp7@users.noreply.github.com>
2024-02-10 10:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.impute import SimpleImputer
 
 # Load the saved model and scaler
 model = pickle.load(open('model.sav', 'rb'))
